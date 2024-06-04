82a064918d83ccc20b5f518586a0469662fcd652
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-04 12:23:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { NavBar } from './components/NavBar'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner.js';
+import { Skills } from './components/Skills.js'
+
 
 function App(){
   return(
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   )
 }
